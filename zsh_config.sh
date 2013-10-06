39ca159b8d27b6ef6856fe8cae0300a6a0ff9b47
ChunxiangYin <root@zw_52_71.(none)>
2013-10-07 01:30:24 +0800

    add install oh-my-zsh

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,3 +1,15 @@
-sudo chsh /usr/loca/bin/zsh
+#install zsh
+wget "http://downloads.sourceforge.net/project/zsh/zsh/5.0.2/zsh-5.0.2.tar.gz"
+tar xzvf zsh-5.0.2.tar.gz
+cd zsh-5.0.2
+./configure --prefix=/usr/local
+make
+sudo make install
+
+#install oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+#update config
+sudo chsh -s /usr/local/bin/zsh
 mv ~/.zshrc ./zshrc.old
-ln -s ~/.zshrc ./zshrc
+ln -s ~/.zshrc `pwd`/zshrc
